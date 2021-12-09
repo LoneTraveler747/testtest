fbbf1506c898d627524ae535d3b59bc6fac4586c
Lone_Traveler <isip_m.a.ruseckiy@mpt.ru>
2021-12-09 23:45:56 +0300

    Первый коммит

---
+++ E-mail/Home.xaml.cs
@@ -0,0 +1,141 @@
+﻿using AE.Net.Mail;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Xml.Serialization;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace E_mail
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class Home : Page
+    {
+        public AE.Net.Mail.Imap.Mailbox[] listMailboxes;
+        public ImapClient ic;
+        public AE.Net.Mail.MailMessage[] mm;
+        public Home()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            this.InitializeComponent();
+        }
+
+        public Zapis zapis = new Zapis();
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Zapis));
+            StorageFile storageFile = await storageFolder.CreateFileAsync("DataBaseMailPassword.xml", CreationCollisionOption.OpenIfExists);
+            Stream read = await storageFile.OpenStreamForReadAsync();
+            try
+            {
+                zapis = (Zapis)xmlSerializer.Deserialize(read);
+            }
+            catch { }
+            read.Close();
+
+            ic = new ImapClient("imap." + zapis.mail, zapis.login, zapis.passwordText, AuthMethods.Login, 993, true);
+            mm = ic.GetMessages(0, ic.GetMessageCount(), false);
+
+            foreach (AE.Net.Mail.MailMessage m in mm)
+            {
+                view.Items.Add(m.Subject);
+            }
+            listMailboxes = ic.ListMailboxes(string.Empty, "*");
+            for (int i = 0; i < listMailboxes.Length; i++)
+            {
+                Menu.Items.Add(listMailboxes[i].Name);
+            }
+        }
+
+        private void Menu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ic.SelectMailbox(listMailboxes[Menu.SelectedIndex].Name);
+            view.Items.Clear();
+            mm = ic.GetMessages(0, ic.GetMessageCount(), false);
+
+            foreach (AE.Net.Mail.MailMessage m in mm)
+            {
+                view.Items.Add(m.Subject);
+            }
+        }
+
+        private void latter_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            latter.Text = "";
+            if (view.SelectedIndex != -1)
+            {
+                latter.Text = mm[view.SelectedIndex].Body;
+            }
+
+        }
+        public bool click = false;
+        private void WriteLatter_Click(object sender, RoutedEventArgs e)
+        {
+            latter.Text = "";
+            if (click == true)
+            {
+                GridLatterView.Visibility = Visibility.Visible;
+                GridLatterWrite.Visibility = Visibility.Collapsed;
+                click = false;
+            }
+            else
+            {
+                click = true;
+                GridLatterWrite.Visibility = Visibility.Visible;
+                GridLatterView.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void AddLatterToSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (user.Text != "" && teg.Text != "" && LatterText.Text != "")
+            {
+                MailAddress from = new MailAddress(zapis.login);
+                // кому отправляем
+                MailAddress to = new MailAddress(user.Text);
+                // создаем объект сообщения
+                System.Net.Mail.MailMessage m = new System.Net.Mail.MailMessage(from, to);
+                // тема письма
+                m.Subject = teg.Text;
+                // текст письма
+                m.Body = LatterText.Text;
+                // письмо представляет код html
+                m.IsBodyHtml = true;
+                // адрес smtp-сервера и порт, с которого будем отправлять письмо
+                SmtpClient smtp = new SmtpClient("smtp." + zapis.mail, 587);
+                // логин и пароль
+                smtp.Credentials = new NetworkCredential(zapis.login, zapis.passwordText);
+                smtp.EnableSsl = true;
+                smtp.Send(m);
+
+                user.Text = "";
+
+                teg.Text = "";
+
+                LatterText.Text = "";
+
+                GridLatterWrite.Visibility = Visibility.Collapsed;
+                GridLatterView.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ E-mail/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using AE.Net.Mail;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Xml.Serialization;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace E_mail
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    [Serializable]
+    public class Zapis
+    {
+        public string mail { get; set; }
+        public string login { get; set; }
+        public string passwordText { get; set; }
+        public Zapis(string mail, string login, string passwordText)
+        {
+            this.mail = mail;
+            this.login = login;
+            this.passwordText = passwordText;
+        }
+        public Zapis()
+        {
+
+        }
+    }
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            this.InitializeComponent();
+        }
+        private async void InPut_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ImapClient ic = new ImapClient("imap.mail.ru", login.Text.ToString(), password.Password.ToString(), AuthMethods.Login, 993, true);
+
+                StorageFolder folder = ApplicationData.Current.LocalFolder;
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Zapis));
+                StorageFile file = await folder.CreateFileAsync("DataBaseMailPassword.xml", CreationCollisionOption.ReplaceExisting);
+                Zapis zapis = new Zapis("mail.ru", login.Text.ToString(), password.Password.ToString());
+                Stream write = await file.OpenStreamForWriteAsync();
+                xmlSerializer.Serialize(write, zapis);
+                write.Close();
+
+                this.Frame.Navigate(typeof(Home));
+            }
+            catch
+            {
+                var rep = await new MessageDialog("Ошибка").ShowAsync();
+            }
+        }
+    }
+}
